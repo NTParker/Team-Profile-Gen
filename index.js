618579290c8e8e8b666c7ada314be2333fe819e0
Noah Parker <noahtmparker@gmail.com>
2022-09-09 10:45:36 -0400

    created all classes/manager function/trying out switch function for menu

--- index.js
+++ index.js
@@ -1,7 +1,100 @@
 const inquirer = require("inquirer");
-const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
 
-let employees = [];
+let team = [];
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "Please enter your ID",
+        name: "id",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter your ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "Please enter your email address",
+        name: "email",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter your email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: "Please enter your office number",
+        name: "officeNumber",
+        validate: (officeInput) => {
+          if (officeInput) {
+            return true;
+          } else {
+            console.log("Please enter your office number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      team.push(manager);
+      promptMenu();
+    });
+}
+
+function promptMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please select what you would like to do:",
+        name: "menu",
+        choices: ["Add and Engineer", "Add an Intern", "Finish my team"],
+      },
+    ])
+    .then((userPrompt) => {
+      switch (userPrompt.menu) {
+        case "Add an Engineer":
+          promptEngineer();
+          break;
+        case "Add an Intern":
+          promptIntern();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+}
 
 function createEmployee() {
   inquirer.prompt([
